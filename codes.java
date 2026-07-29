f9eddc889a3cc04befea6abab237cb593a3bec83
Sabyasachi <krram368@gmail.com>
2026-07-29 22:28:15 +0530

    LOOPS

---
+++ codes.java
@@ -0,0 +1,121 @@
+package loops;
+
+import java.util.*;
+
+
+public class codes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+// Q- print this series - 99,95,91,87,.... upto all terms which are positive
+// method - 1
+//for (int i=99;i>0;i-=4)  {
+//    System.out.print("The term is "+ i );
+//    System.out.println();
+//}
+//
+// // method-2
+//        int a = 99 , d = 4;
+//        for (int i =0; i<25;i++){
+//            System.out.print(a+" ");
+//            a-=d;
+//        }
+
+        //Question
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int a=1 , r = 2;
+//        for(int i=1; i<=n;i++){
+//            System.out.println(a);
+//            a*=r;
+//        }
+
+        //Question
+//       for (int i=67; i<=90; i++){
+//           System.out.println((char)i +"-"+i);
+//       }
+
+        //Question
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int temp = n;
+//        for (int i=1;i<=n/2;i++){
+//            System.out.println(i);
+//            System.out.println(temp);
+//            temp--;
+//        }
+
+
+//Question-prime number
+
+//        int n = sc.nextInt();
+//        boolean flag = true;
+//
+//        for (int i=2;i<=Math.sqrt(n);i++){
+//            if (n%i==0) {
+//                flag = false;
+//                System.out.println(i);
+//                if (i == (n / i)) {
+//                    System.out.println(i);
+//                } else {
+//                    System.out.println(n / i);
+//
+//                }
+//                break;
+//            }
+//        }
+//        if(n==1){
+//            System.out.println("Neither prime nor odd");
+//        } else if (flag == false) {
+//            System.out.println("Composite Number");
+//        }else{
+//            System.out.println("prime number");
+//        }
+//
+
+
+        //Question--
+
+//        System.out.println("Enter the number:  ");
+//        int n = sc.nextInt();
+//        if (n==0){
+//            n=1;
+//        }
+//        int count=0;
+//        while(n!=0){
+//            n/=10;
+//            count++;
+//        }
+//        System.out.println(count);
+//
+//        System.out.println("Enter the number:  ");
+//        int n = sc.nextInt();
+//        int sum=0;
+//        int count=0;
+//        int rev=0;
+//        int x =0;
+//        int y=n;
+//
+//        while(n!=0){
+//            rev*=10;
+//            rev +=(n%10);
+//
+//            sum+=(n%10);
+//            n/=10;
+//            count++;
+//            x = rev;
+//        }
+//        System.out.println(count);
+//        System.out.println(sum>0 ? sum :  -sum);
+//        System.out.println(x);
+//
+//        System.out.println(x+y);
+
+
+
+//Question---
+//        long x = Long.MAX_VALUE;
+//        System.out.println(x);
+
+    }
+}
