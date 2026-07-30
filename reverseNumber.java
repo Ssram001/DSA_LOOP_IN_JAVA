c23a5e81b8665b409cba9b4352116bedb6984b87
Sabyasachi <krram368@gmail.com>
2026-07-30 22:11:33 +0530

    revNum

---
+++ reverseNumber.java
@@ -0,0 +1,46 @@
+package loops;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question--
+
+//        System.out.println("Enter the number:  ");
+//        int n = sc.nextInt();
+//        if (n==0){
+//            n=1;
+//        }
+//        int count=0;
+//        while(n!=0){
+//            n/=10;
+//            count++;
+//        }
+//        System.out.println(count);
+//
+//        System.out.println("Enter the number:  ");
+//        int n = sc.nextInt();
+//        int sum=0;
+//        int count=0;
+//        int rev=0;
+//        int x =0;
+//        int y=n;
+//
+//        while(n!=0){
+//            rev*=10;
+//            rev +=(n%10);
+//
+//            sum+=(n%10);
+//            n/=10;
+//            count++;
+//            x = rev;
+//        }
+//        System.out.println(count);
+//        System.out.println(sum>0 ? sum :  -sum);
+//        System.out.println(x);
+//
+//        System.out.println(x+y);
+
+    }
+}
