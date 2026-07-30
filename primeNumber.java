35a03751e7ce527d5d659ac5192a3b45c67395e7
Sabyasachi <krram368@gmail.com>
2026-07-30 22:11:12 +0530

    primeNum

---
+++ primeNumber.java
@@ -0,0 +1,35 @@
+package loops;
+
+import java.util.Scanner;
+
+public class primeNumber {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Question-prime number
+
+//        int n = sc.nextInt();
+//        boolean flag = true;
+//
+//        for (int i=2;i<=Math.sqrt(n);i++){
+//            if (n%i==0) {
+//                flag = false;
+//                System.out.println(i);
+//                if (i == (n / i)) {
+//                    System.out.println(i);
+//                } else {
+//                    System.out.println(n / i);
+//
+//                }
+//                break;
+//            }
+//        }
+//        if(n==1){
+//            System.out.println("Neither prime nor odd");
+//        } else if (flag == false) {
+//            System.out.println("Composite Number");
+//        }else{
+//            System.out.println("prime number");
+//        }
+//
+    }
+}
